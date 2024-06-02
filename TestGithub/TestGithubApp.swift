be320f974a96ee808ee8f1c555a305b716734a67
LearnSwiftProgramming <beadeveloper@gmail.com>
2024-06-02 21:44:43 +0530

    Initial Commit

---
+++ TestGithub/TestGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGithubApp.swift
+//  TestGithub
+//
+//  Created by Rahul Ajmera on 02/06/24.
+//
+
+import SwiftUI
+
+@main
+struct TestGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
